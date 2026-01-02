9f8cccd5b03359d3d06dda6fb4df277a4d11c923
Alexander <alexand312z@hotmail.com>
2026-01-02 14:50:04 -0500

    home page styling on the page listing

--- frontnext/app/page.tsx
+++ frontnext/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -7,17 +8,18 @@ export default function Home() {
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Please visit the pages:
-            <ul>
+            <ul className="list-disc ml-6 text-blue-600 dark:text-blue-400">
               <li>
-                <a
+                <Link className="hover:underline"
                 href="/un"
-                className="text-blue-600 hover:underline dark:text-blue-400"
                 >
                 /un for the first game
-                </a>
+                </Link>
               </li>
               <li>
-                <p>more to come...</p>
+                <p className="text-black dark:text-zinc-300">
+                  ...more to come...
+                  </p>
               </li>
             </ul>
           </h1>
